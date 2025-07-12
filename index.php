a336aae0a105a89e80e8d4e83def0245e7c32542
Yuriy Zinchenko <yurazinch@gmail.com>
2025-07-12 14:50:25 +0300

    все опреации с количеством

--- index.php
+++ index.php
@@ -13,14 +13,14 @@ $operations = [
     OPERATION_DELETE => OPERATION_DELETE . '. Удалить товар из списка покупок.',
     OPERATION_PRINT => OPERATION_PRINT . '. Отобразить список покупок.',
     OPERATION_EDIT => OPERATION_EDIT . '. Изменить товар.',
-    OPERATION_QUANTITY => OPERATION_QUANTITY . '. Добавить количество.'
+    OPERATION_QUANTITY => OPERATION_QUANTITY . '. Изменить количество.'
 ];
 
 $items = [];
 
 function toPrint(string $a): void 
-{
-    echo $a . PHP_EOL;
+{    
+    echo PHP_EOL . $a . PHP_EOL;    
 }
 
 function toString(array $b): void
@@ -28,14 +28,53 @@ function toString(array $b): void
     echo implode(PHP_EOL, $b) . PHP_EOL;
 }
 
+function toStringItems(array $items): void
+{
+    foreach ($items as $item => $value) 
+    {
+        toPrint("Наименование: {$item}, шт.: {$value}");
+    }
+}
+
+function addItem(string $a): void
+{
+    global $items;
+    
+    $toAdd = explode(',', $a);
+    $item = (string)trim($toAdd[0]);
+    if(array_key_exists(1, $toAdd) !== false)
+    {
+        $value = trim($toAdd[1]);
+    }
+    else
+    {
+        $value = 0;
+    }
+    if (is_numeric($value) !== false)
+    {
+        $items[$item] = (int)$value;
+        if($items[$item] < 0)
+        {
+            $items[$item] = 0;
+        }
+    }
+    else
+    {
+        toPrint('Правильно укажите количество');
+    }
+}
+
 function deleteItem(string $c): void // удаление товара
 {
     global $items;
 
-    if (in_array($c, $items, true) !== false) {
-        while (($key = array_search($c, $items, true)) !== false) {
-            unset($items[$key]);
-        }
+    if (array_key_exists(trim($c), $items) !== false) 
+    {        
+        unset($items[trim($c)]);      
+    }
+    else
+    {
+        toPrint('Введите правильное название');
     }
 }
 
@@ -43,38 +82,60 @@ function editItem(string $c): void // изменение названия
 {
     global $items;
 
-    $toChange = explode(',', $c);            
-    if(in_array($toChange[0], $items, true) !== false) {
-        while (($key = array_search(trim($toChange[0]), $items, true)) !== false) {
-            $items[$key] = trim($toChange[1]);
+    $toChange = explode(',', $c); 
+    $key = (string)trim($toChange[0]);
+    $item = trim($toChange[1]);
+    if(array_key_exists($key, $items) !== false) 
+    {
+        unset($items[$key]);
+        toPrint('Введите количество для добавления в список:');
+        $value = trim(fgets(STDIN));
+        if (is_numeric($value) === true)
+        {
+            $items[$item] = (int)$value;
+            if($items[$item] < 0)
+            {
+                $items[$item] = 0;
+            }
+        }
+        else
+        {
+            toPrint('Укажите количество числом');
         }
     }
+
 }
 
-function addQuantity(string $c): void // добавить количество
+function addQuantity(string $c): void // изменить количество
 {
 
     global $items;
 
     $toQuantity = explode(',', $c);
-    if(is_numeric(trim($toQuantity[1])) === true) {
-        $toQuantity[1] = (int)trim($toQuantity[1]);                
-        if(in_array(trim($toQuantity[0]), $items, true) !== false) {            
-            while (($key = array_search(trim($toQuantity[0]), $items, true)) === false) {
-                $items[$key] = trim($toQuantity[0]) . ": штук: $toQuantity[1]";
-                /*if(is_array($items[$key]) !== false) {
-                    $items[$key][$toQuantity[0]] += $toQuantity[1];                            
-                } else {
-                    $items[$key] = [trim($toQuantity[0]) => $toQuantity[1]];
-                }*/   
-            } 
-        } else {
-            toPrint('Такого товара нет в списке.');
+    $item = trim($toQuantity[0]);
+    if($toQuantity[1])
+    $value = trim($toQuantity[1]);
+    if(array_key_exists($item, $items) !== false)
+    {
+        if(is_numeric($value) === true)
+        {
+            $value = (int)$value;
+            $items[$item] += $value;
+            if($items[$item] < 0)
+            {
+                $items[$item] = 0;
+            }
         }
-    } else {
-        toPrint('Количество указано неправильно.');
+        else 
+        {
+            toPrint('Количество указано неправильно.');
+        }
     }
-   
+    else
+    {
+        toPrint('Такого товара нет в списке.');
+    }
+    
 }
 
 
@@ -85,11 +146,10 @@ do {
     do {
         if (count($items)) {
             toPrint('Ваш список покупок: ');
-            toString($items);
+            toStringItems($items);
         } else {
             toPrint('Ваш список покупок пуст.');
         }
-
 
         toPrint('Выберите операцию для выполнения: ');
         // Проверить, есть ли товары в списке? Если нет, то не отображать пункт про удаление товаров
@@ -108,27 +168,25 @@ do {
 
     switch ($operationNumber) {
         case OPERATION_ADD:
-            toPrint('Введение название товара для добавления в список:');
+            toPrint('Введите название товара и количество через запятую для добавления в список:');
             $itemName = trim(fgets(STDIN));
-            $items[] = $itemName;
+            addItem($itemName);            
             break;
 
         case OPERATION_DELETE:
             // Проверить, есть ли товары в списке? Если нет, то сказать об этом и попросить ввести другую операцию
             toPrint('Текущий список покупок:');
             toPrint('Список покупок: ');
-            toString($items);
-
+            toStringItems($items);
             toPrint('Введение название товара для удаления из списка:');
             echo '> ';
             $itemName = trim(fgets(STDIN));
-            deleteItem($itemName);
-            
+            deleteItem($itemName);            
             break;
 
         case OPERATION_PRINT:
             toPrint('Ваш список покупок: ');
-            toString($items);
+            toStringItems($items);
             $count = count($items);
             $n = "Всего  $count позиций.";
             toPrint($n);
@@ -141,7 +199,7 @@ do {
             echo '> ';
             $names = trim(fgets(STDIN));
             editItem($names);
-            
+            print_r($items);
             break;            
 
         case OPERATION_QUANTITY:
@@ -149,7 +207,7 @@ do {
             echo '> ';
             $withQuantity = trim(fgets(STDIN));
             addQuantity($withQuantity);
-                        
+            print_r($items);            
             break; 
     }
 
